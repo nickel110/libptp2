e0e8e6dee5b3a66b49ffbbec7d49b9792589ea91
Thomas Villeboeuf <Vous@exemple.com>
2022-11-12 15:50:43 +0100

    fix prototype and declaration arg type

--- src/ptp.h
+++ src/ptp.h
@@ -745,7 +745,7 @@ typedef uint16_t (* PTPIOSendData)	(PTPParams* params, PTPContainer* ptp,
 					unsigned char *data, unsigned int size);
 typedef uint16_t (* PTPIOGetResp)	(PTPParams* params, PTPContainer* resp);
 typedef uint16_t (* PTPIOGetData)	(PTPParams* params, PTPContainer* ptp,
-					uint32_t *getlen,
+					uint64_t *getlen,
 					unsigned char **data);
 /* debug functions */
 typedef void (* PTPErrorFunc) (void *data, const char *format, va_list args);
